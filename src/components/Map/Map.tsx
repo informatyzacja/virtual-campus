700d549e87995e1e377ca97a8530c8bac46d0d27
Michał Zychowicz <zychomichal@gmail.com>
2023-04-28 21:34:33 +0200

    feat: Added basic leaflet and OSM integration
    feat: Added basic GeoJSON example
    
    feat: Added basic GeoJSON popup example

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,40 @@
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+import type { GeoJsonObject } from "geojson";
+import type { Layer } from "leaflet";
+import { GeoJSON, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+import mapData from "@/components/Map/mapData.json";
+
+
+const Map = () => {
+  return (
+    <MapContainer center={[51.10888547242358, 17.060051620079904]} zoom={17} scrollWheelZoom={true}
+                  style={{ height: "100%", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <GeoJSON onEachFeature={onEachFeature} data={mapData as GeoJsonObject} />
+
+      <Marker
+        position={[51.1091064802161, 17.06046003426282]}
+        draggable={false}
+        // animate={true}
+      >
+        <Popup>C-1</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+function onEachFeature(feature: GeoJSON.Feature, layer: Layer) {
+  if (feature.properties?.name) {
+    layer.bindPopup(feature.properties.name as string);
+  }
+}
+
+export default Map;
